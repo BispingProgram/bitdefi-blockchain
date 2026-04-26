38c728f30088d36f21fef8f1ddc9ab081848f36d
BISPO CORP <134176541+BispingProgram@users.noreply.github.com>
2026-04-25 21:45:40 -0300

    Add files via upload

--- models.py
+++ models.py
@@ -5,6 +5,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True)
-    password = Column(String)
-    address = Column(String)
+
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    address = Column(String(255), nullable=False)
